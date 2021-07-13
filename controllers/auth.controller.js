f8926aa7de2a2cc54d80a18feb5b7e7d4822b893
arnaud.jouanne <arnaud.jouanne89@gmail.com>
2021-07-13 19:29:25 +0200

    Still working on the app security with JWT

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,14 @@
 const UserModel = require('../models/user.model');
 const jwt = require('jsonwebtoken');
 
+const maxAge = 3 * 24 * 60 * 60 * 1000;
+
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.TOKEN_SECRET, {
+        expiresIn: maxAge
+    })
+};
+
 module.exports.signUp = async (req, res) => {
     const {pseudo, email, password} = req.body
 
@@ -19,8 +27,10 @@ module.exports.signIn = async (req, res) => {
     try {
         const user = await UserModel.login(email, password);
         const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge });
+        res.status(200).json({ user: user._id })
     } catch (err) {
-
+        res.status(400).json(err);
     }
 }
 
