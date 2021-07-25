a6dff6175a86f77243990316ba22a9dc083cf983
arnaud.jouanne <arnaud.jouanne89@gmail.com>
2021-07-25 17:24:26 +0200

    Finish authentication part, start working on the navbar component

--- client/src/components/Log/SignUpForm.js
+++ client/src/components/Log/SignUpForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import SignInForm from "./SignInForm";
 
 const SignUpForm = () => {
   const [formSubmit, setFormSubmit] = useState(false);
@@ -54,62 +55,74 @@ const SignUpForm = () => {
   };
 
   return (
-    <form action="" onSubmit={handleRegister} id="sign-up-form">
-      <label htmlFor="pseudo">Pseudo</label>
-      <br />
-      <input
-        type="text"
-        name="pseudo"
-        id="pseudo"
-        onChange={(e) => setPseudo(e.target.value)}
-        value={pseudo}
-      />
-      <div className="pseudo error"></div>
-      <br />
-      <label htmlFor="email">Email</label>
-      <br />
-      <input
-        type="text"
-        name="email"
-        id="email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      />
-      <div className="email error"></div>
-      <br />
-      <label htmlFor="password">Mot de passe</label>
-      <br />
-      <input
-        type="password"
-        name="password"
-        id="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      />
-      <div className="password error"></div>
-      <br />
-      <label htmlFor="password-conf">Confirmer mot de passe</label>
-      <br />
-      <input
-        type="password"
-        name="password"
-        id="password-conf"
-        onChange={(e) => setControlPassword(e.target.value)}
-        value={controlPassword}
-      />
-      <div className="password-confirm error"></div>
-      <br />
-      <input type="checkbox" id="terms" />
-      <label htmlFor="terms">
-        J'accepte les{" "}
-        <a href="/" target="_blank" rel="noopener noreferrer">
-          conditions générales
-        </a>
-      </label>
-      <div className="terms error"></div>
-      <br />
-      <input type="submit" value="Valider Inscription" />
-    </form>
+    <>
+      {formSubmit ? (
+        <>
+          <SignInForm />
+          <span></span>
+          <h4 className="success">
+            Enregistrement réussi, veuillez-vous connecter
+          </h4>
+        </>
+      ) : (
+        <form action="" onSubmit={handleRegister} id="sign-up-form">
+          <label htmlFor="pseudo">Pseudo</label>
+          <br />
+          <input
+            type="text"
+            name="pseudo"
+            id="pseudo"
+            onChange={(e) => setPseudo(e.target.value)}
+            value={pseudo}
+          />
+          <div className="pseudo error"></div>
+          <br />
+          <label htmlFor="email">Email</label>
+          <br />
+          <input
+            type="text"
+            name="email"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <div className="email error"></div>
+          <br />
+          <label htmlFor="password">Mot de passe</label>
+          <br />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <div className="password error"></div>
+          <br />
+          <label htmlFor="password-conf">Confirmer mot de passe</label>
+          <br />
+          <input
+            type="password"
+            name="password"
+            id="password-conf"
+            onChange={(e) => setControlPassword(e.target.value)}
+            value={controlPassword}
+          />
+          <div className="password-confirm error"></div>
+          <br />
+          <input type="checkbox" id="terms" />
+          <label htmlFor="terms">
+            J'accepte les{" "}
+            <a href="/" target="_blank" rel="noopener noreferrer">
+              conditions générales
+            </a>
+          </label>
+          <div className="terms error"></div>
+          <br />
+          <input type="submit" value="Valider Inscription" />
+        </form>
+      )}
+    </>
   );
 };
 
--- client/src/components/Routes/index.js
+++ client/src/components/Routes/index.js
@@ -3,10 +3,12 @@ import Home from '../../pages/Home';
 import Profil from '../../pages/Profil';
 import Trending from '../../pages/Trending';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import Navbar from '../Navbar';
 
 const index = () => {
   return (
     <Router>
+      <Navbar />
         <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/profil" exact component={Profil} />
