43b30f6fe125fe01a69dfaa0057a28d87986ed92
arnaud.jouanne <arnaud.jouanne89@gmail.com>
2021-07-26 18:40:37 +0200

    Still working on the profil page, add the two pop-up when clicking on abonnés & abonnements buttons

--- client/src/components/Profil/UpdateProfil.js
+++ client/src/components/Profil/UpdateProfil.js
@@ -9,14 +9,15 @@ const UpdateProfil = () => {
   const [bio, setBio] = useState("");
   const [updateForm, setUpdateForm] = useState(false);
   const userData = useSelector((state) => state.userReducer);
+  const usersData = useSelector((state) => state.usersReducer);
   const dispatch = useDispatch();
   const [followingPopup, setFollowingPopup] = useState(false);
-  const [followersPopup, setFollowersfPopup] = useState(false);
+  const [followersPopup, setFollowersPopup] = useState(false);
 
   const handleUpdate = () => {
     dispatch(updateBio(userData._id, bio));
     setUpdateForm(false);
-  }
+  };
 
   return (
     <div className="profil-container">
@@ -51,10 +52,64 @@ const UpdateProfil = () => {
             )}
           </div>
           <h4>Membre depuis le : {dateParser(userData.createdAt)}</h4>
-          <h5>Abonnements : {userData.following ? userData.following.length : ""}</h5>
-          <h5>Abonnés : {userData.followers ? userData.followers.length : ""}</h5>
+          <h5 onClick={() => setFollowingPopup(true)}>
+            Abonnements : {userData.following ? userData.following.length : ""}
+          </h5>
+          <h5 onClick={() => setFollowersPopup(true)}>
+            Abonnés : {userData.followers ? userData.followers.length : ""}
+          </h5>
         </div>
       </div>
+      {followersPopup && (
+        <div className="popup-profil-container">
+          <div className="modal">
+            <h3>Abonnés</h3>
+            <span className="cross" onClick={() => setFollowersPopup(false)}>
+              &#10005;
+            </span>
+            <ul>
+              {usersData.map((user) => {
+                for (let i = 0; i < userData.followers.length; i++) {
+                  if (user._id === userData.followers[i]) {
+                    return (
+                      <li key={user._id}>
+                        <img src={user.picture} alt="user-pic" />
+                        <h4>{user.pseudo}</h4>
+                        <h1>FOLLOW HANDLER</h1>
+                      </li>
+                    );
+                  }
+                }
+              })}
+            </ul>
+          </div>
+        </div>
+      )}
+      {followingPopup && (
+        <div className="popup-profil-container">
+          <div className="modal">
+            <h3>Abonnements</h3>
+            <span className="cross" onClick={() => setFollowingPopup(false)}>
+              &#10005;
+            </span>
+            <ul>
+              {usersData.map((user) => {
+                for (let i = 0; i < userData.following.length; i++) {
+                  if (user._id === userData.following[i]) {
+                    return (
+                      <li key={user._id}>
+                        <img src={user.picture} alt="user-pic" />
+                        <h4>{user.pseudo}</h4>
+                        <h1>FOLLOW HANDLER</h1>
+                      </li>
+                    );
+                  }
+                }
+              })}
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
