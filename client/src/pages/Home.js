b9d90958d29b4444998ca32ca64d96feb5c11e4d
arnaud.jouanne <arnaud.jouanne89@gmail.com>
2021-08-02 16:53:42 +0200

    Finish working on the trending component + create, add & finish the FriendsHint component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react';
-import { UidContext } from '../components/AppContext';
-import LeftNav from '../components/LeftNav';
-import NewPostForm from '../components/Post/NewPostForm';
-import Thread from '../components/Thread';
-import Log from '../components/Log';
-import Trends from '../components/Trends';
+import React, { useContext } from "react";
+import { UidContext } from "../components/AppContext";
+import LeftNav from "../components/LeftNav";
+import NewPostForm from "../components/Post/NewPostForm";
+import Thread from "../components/Thread";
+import Log from "../components/Log";
+import Trends from "../components/Trends";
+import FriendsHint from "../components/Profil/FriendsHint";
 
 const Home = () => {
   const uid = useContext(UidContext);
@@ -22,6 +23,7 @@ const Home = () => {
         <div className="right-side-container">
           <div className="wrapper">
             <Trends />
+            {uid && <FriendsHint />}
           </div>
         </div>
       </div>
